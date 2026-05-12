e9db816a1bc2a932cdad347487b6ab770c2c0a1b
Mekito4u <maks_epifanov_2017@mail.ru>
2026-05-12 23:45:07 +0300

    feat: update Routes&Controllers

--- app/src/main/java/com/app/MainActivity.kt
+++ app/src/main/java/com/app/MainActivity.kt
@@ -8,8 +8,13 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.app.ui.views.*
 import com.app.ui.theme.DubleeTheme
+import com.app.ui.views.CategoryView
+import com.app.ui.views.CreatePairView
+import com.app.ui.views.JoinPairView
+import com.app.ui.views.LoginView
+import com.app.ui.views.MainView
+import com.app.ui.views.RegisterView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,31 +29,37 @@ class MainActivity : ComponentActivity() {
 }
 
 enum class Nav(val route: String) {
+    // Основные
     Main("main"),
-    Login("login"),
-    Register("register"),
-    CreatePair("create_pair"),
-    JoinPair("join_pair"),
-    CategorySelection("category_selection"),
     Likes("likes"),
     Stats("stats"),
     Profile("profile"),
+
+    // Авторизация
+    Login("login"),
+    Register("register"),
+
+    // Пары
+    CreatePair("create_pair"),
+    JoinPair("join_pair"),
+
+    // Дополнительные
+    Settings("settings")
 }
 
 @Composable
 fun AppNavigation() {
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = Nav.CategorySelection.route) {
+    NavHost(navController = navController, startDestination = Nav.Login.route) {
         composable(Nav.Main.route) { MainView(navController) }
         composable(Nav.Login.route) { LoginView(navController) }
         composable(Nav.Register.route) { RegisterView(navController) }
         composable(Nav.CreatePair.route) { CreatePairView(navController) }
         composable(Nav.JoinPair.route) { JoinPairView(navController) }
-        composable(Nav.CategorySelection.route) { CategorySelectionView(navController) }
-//        composable("category/{categoryId}") {
-//            val categoryId = it.arguments?.getString("categoryId")
-//            SwipeView(navController, categoryId)
-//        }
+        composable("category/{categoryId}") { backStackEntry ->
+            val categoryId = backStackEntry.arguments?.getString("categoryId")
+            CategoryView(navController, categoryId?.toIntOrNull() ?: 1)
+        }
     }
 }
