ab12e528c648b042b74f646c0ded278a2e95c836
Mekito4u <maks_epifanov_2017@mail.ru>
2026-05-12 23:46:53 +0300

    feat: add Main: V&VM

---
+++ app/src/main/java/com/app/ui/viewmodel/MainViewModel.kt
@@ -0,0 +1,22 @@
+package com.app.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.app.data.repositories.CategoryRepository
+import com.app.domain.models.CategoryModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class MainViewModel() : ViewModel() {
+    val repository = CategoryRepository()
+
+    private val _categoryList = MutableStateFlow(emptyList<CategoryModel>())
+    val categoryList = _categoryList.asStateFlow()
+
+    init {
+        updateCategory()
+    }
+
+    fun updateCategory() {
+        _categoryList.value = repository.getCategories()
+    }
+}
--- app/src/main/java/com/app/ui/views/MainView.kt
+++ app/src/main/java/com/app/ui/views/MainView.kt
@@ -1,21 +1,74 @@
 package com.app.ui.views
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.app.ui.theme.MyBeige
+import com.app.ui.theme.MyText
+import com.app.ui.viewmodel.MainViewModel
+import com.app.ui.widgets.CategoryWidget
+import com.app.ui.widgets.DubleeIconWidget
+import com.app.ui.widgets.DubleeWidget
 
 @Preview
 @Composable
 fun MainView(
-    navController: NavController = rememberNavController()
+    navController: NavController = rememberNavController(),
+    viewModel: MainViewModel = viewModel()
 ) {
-    BaseView(
-        modifier = Modifier.background(MyBeige),
-    ) {
+    val categoryList by viewModel.categoryList.collectAsStateWithLifecycle()
 
+    BaseView {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            item { DubleeWidget() }
+            item {
+                Spacer(
+                    modifier = Modifier
+                        .height(64.dp)
+                )
+            }
+            item { DubleeIconWidget() }
+            item {
+                Spacer(
+                    modifier = Modifier
+                        .height(64.dp)
+                )
+            }
+            item { MyText(text = "Выберите категорию:", style = typography.titleLarge) }
+            items(categoryList) { item ->
+                CategoryWidget(
+                    title = item.title,
+                    description = item.description,
+                    icon = item.icon,
+                    onClicked = {
+                        navController.navigate("category/${item.id}")
+                    }
+                )
+            }
+            item {
+                Spacer(
+                    modifier = Modifier
+                        .height(0.dp)
+                )
+            }
+        }
     }
 }
