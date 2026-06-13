cd7bf7990788f69174889690d291900d1cd630f9
Mekito4u <maks_epifanov_2017@mail.ru>
2026-06-13 14:50:14 +0300

    feat: add new methods

--- app/src/main/java/com/dublee/app/domain/usecases/PartnerSession.kt
+++ app/src/main/java/com/dublee/app/domain/usecases/PartnerSession.kt
@@ -2,16 +2,19 @@ package com.dublee.app.domain.usecases
 
 import com.dublee.app.domain.models.UserModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
-open class PartnerSession() {
+class PartnerSession() {
     private val emptyPartner = UserModel.emptyPair().second
     private val _partner = MutableStateFlow(emptyPartner)
-    open val partner: StateFlow<UserModel> = _partner.asStateFlow()
+    val partner = _partner.asStateFlow()
 
 
-    fun loadPartner(partner: UserModel) {
-        _partner.value = partner
+    fun updatePartner(user: UserModel) {
+        _partner.value = user
+    }
+
+    fun clearPartner() {
+        _partner.value = emptyPartner
     }
 }
--- app/src/main/java/com/dublee/app/domain/usecases/UserSession.kt
+++ app/src/main/java/com/dublee/app/domain/usecases/UserSession.kt
@@ -4,13 +4,17 @@ import com.dublee.app.domain.models.UserModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
-open class UserSession() {
+class UserSession() {
     private val emptyUser = UserModel.emptyPair().first
     private val _user = MutableStateFlow(emptyUser)
-    open val user = _user.asStateFlow()
+    val user = _user.asStateFlow()
 
 
-    fun loadUser(user: UserModel) {
+    fun updateUser(user: UserModel) {
         _user.value = user
+    }
+
+    fun clearUser() {
+        _user.value = emptyUser
     }
 }
